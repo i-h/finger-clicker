22b068018e9f8386ac96c8d12b7871f65d7da85b
KOUDATA\teemu.kokkonen <teemu.kokkonen@koudata.fi>
2016-11-07 15:28:11 +0200

    Initial commit

---
+++ Assets/Resources/Scripts/ClickScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+public class ClickScript : MonoBehaviour {
+    public static int Score = 0;
+    public Text ScoreDisplay;
+    void OnMouseDown()
+    {
+        Score++;
+        if (ScoreDisplay)
+        {
+            ScoreDisplay.text = Score.ToString();
+        }
+        if(Score == 1)
+        {
+            Social.Active.ReportProgress(FingerClicker.GPGSIds.achievement_first_click, 100.0f, (bool success) => {
+                Debug.Log("ACHIEVEMENT POST SUCCESS: " + success);
+            });
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class GameManager : MonoBehaviour {
+    public int logins;
+    void Awake()
+    {
+        Social.localUser.Authenticate((bool success) => {
+            Debug.Log("Login success: " + success + "\n" + Social.localUser.userName);
+            if (success)
+            {
+                Debug.Log("Google username: " + Social.localUser.userName);
+
+
+            }
+        });
+        // Google Play config 
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+        // enables saving game progress.
+        .EnableSavedGames()
+        .Build();
+
+        Debug.Log("Google username: " + Social.localUser.userName);
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
+
+    }
+    void Start()
+    {
+        Social.Active.ShowAchievementsUI();
+    }
+}
